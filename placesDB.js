d66592768e5e2d485e09b28587a1bf9bf10868ce
jchlus <jchlus@stevens.edu>
2017-11-11 18:07:25 -0500

    commit

--- placesDB.js
+++
@@ -1,7 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/resdb";
-
-MongoClient.connect(url, function(err, db) {
-if(err) throw err;
-console.log("Database created!");
-});
