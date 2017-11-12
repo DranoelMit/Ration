99ba5e2b052b6352f2bb5dd8b75319bcae82f585
rkondrat <rkondrat@stevnes.edu>
2017-11-12 15:55:03 -0500

    Fix7

--- dispHelper.js
+++ dispHelper.js
@@ -1,6 +1,12 @@
 function displayValues(name, location, hours)
 {
+
+	document.getElementById('money').style.display ="none";
+	document.getElementById('moneysign').style.display ="none";
+
   document.getElementById('nameDB').innerHTML = name;
   document.getElementById('locationDB').innerHTML = location;
   document.getElementById('hoursDB').innerHTML = hours;
+
+
 }
